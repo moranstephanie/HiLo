f305bfb8a3776de514ebbf8bc62fbfbfe10e8dc5
stephanie <moranstephanie83@gmail.com>
2017-03-28 14:21:55 +0000

    put project name here

--- script.js
+++ script.js
@@ -4,16 +4,18 @@
 $(document).ready(function () {
   // All code goes here
   var correctnumber = "7";
-
   $("#life").click(function(){
 
   if($("#userinput").val() === correctnumber){
        alert("Congratulations!");
         
   }
-   else{
-        alert("WRONG");
+   else if ($("#userinput").val() > correctnumber){
+        alert("Too High");
     }
+     else if ($("#userinput").val() < correctnumber){
+        alert("Too Low");
+     }
 });
   
 });
